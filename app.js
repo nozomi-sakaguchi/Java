e0253e8568ab5de4a3742bbef44a2d3f40c1ebab
nozomi-sakaguchi <you@example.com>
2023-09-28 10:05:42 +0000

    9/28

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-var pattern = /^.{8,}$/;
-if (pattern.test('1234abcd')) {
-  console.log('OK');
-} else {
-  console.log('NG');
-}
+var updateButton = function() {
+  if ($(window).scrollTop() >= 300) {
+    $('.back-to-top').fadeIn();
+  } else {
+    $('.back-to-top').fadeOut();
+  }
+};
+
+$(window).on('scroll', updateButton);
+
+$('.back-to-top').on('click',function(e){
+  e.preventDefault();
+
+  $('html,body').animate({scrollTop: 0}, 600);
+});
+
+updateButton();
